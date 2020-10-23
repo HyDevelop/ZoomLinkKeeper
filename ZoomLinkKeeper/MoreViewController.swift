5761785ef80314484c8edca74dfffba0ae705b2f
Hykilpikonna <me@hydev.org>
2020-10-23 10:59:30 -0400

    [+] Create outlets for text fields

---
+++ ZoomLinkKeeper/MoreViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ZoomLinkKeeper
+//
+//  Created by Hykilpikonna on 10/22/20.
+//
+
+import UIKit
+
+class MoreViewController: UIViewController
+{
+    let userDefaults = UserDefaults.standard
+    
+    @IBOutlet weak var calendarUrl: UITextField!
+    @IBOutlet weak var regex: UITextField!
+}
