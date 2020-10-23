dfdf68d51dc7d2108e85c61709d8dd89a3a76934
Hykilpikonna <me@hydev.org>
2020-10-23 13:37:48 -0400

    [+] Create link table cell class

---
+++ ZoomLinkKeeper/LinkTableCell.swift
@@ -0,0 +1,14 @@
+//
+//  LinkTableCell.swift
+//  ZoomLinkKeeper
+//
+//  Created by Hykilpikonna on 10/23/20.
+//
+
+import UIKit
+
+class LinkTableCell: UITableViewCell
+{
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var input: UITextField!
+}
