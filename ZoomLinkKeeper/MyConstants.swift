a3187baa2ae61f2fb579c9e137dd7d37ab7f54b9
Hykilpikonna <me@hydev.org>
2020-10-23 10:27:45 -0400

    [+] Create constants

---
+++ ZoomLinkKeeper/MyConstants.swift
@@ -0,0 +1,14 @@
+//
+//  MyConstants.swift
+//  ZoomLinkKeeper
+//
+//  Created by Hykilpikonna on 10/23/20.
+//
+
+import Foundation
+
+struct MyConstants
+{
+    static let defaultCalendarUrl = "https://www.stjohnsprep.org/page.cfm?p=8267&period=day"
+    static let defaultRegex = "(?<=HS - Day )[1-7](?= - )"
+}
