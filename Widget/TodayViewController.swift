08d156d98dccc3bedc51836183e5612aee56a005
Hykilpikonna <me@hydev.org>
2020-10-23 17:54:59 -0400

    [+] Create widget

---
+++ Widget/TodayViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TodayViewController.swift
+//  Widget
+//
+//  Created by Hykilpikonna on 10/23/20.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        completionHandler(NCUpdateResult.newData)
+    }
+}
