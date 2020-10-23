3aa96b7840e9f5123126322e8feb3c3fdbf5b5c4
Hykilpikonna <me@hydev.org>
2020-10-23 10:58:12 -0400

    [+] Store default settings on start

--- ZoomLinkKeeper/AppDelegate.swift
+++ ZoomLinkKeeper/AppDelegate.swift
@@ -8,29 +8,37 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    let userDefaults = UserDefaults.standard
+    
+    func application(_ app: UIApplication, didFinishLaunchingWithOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
         // Override point for customization after application launch.
+        
+        // Initialize settings on fist launch
+        if userDefaults.string(forKey: "calendar-url") == nil
+        {
+            userDefaults.setValue("calendar-url", forKey: MyConstants.defaultCalendarUrl)
+            userDefaults.setValue("regex", forKey: MyConstants.defaultRegex)
+        }
+        
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(_ application: UIApplication, configurationForConnecting: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration
+    {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: configurationForConnecting.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>)
+    {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
-
