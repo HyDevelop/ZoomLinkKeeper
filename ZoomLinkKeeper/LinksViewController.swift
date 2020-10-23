d73bac2bbd1c2879eadf609d20fcef49176d0be7
Hykilpikonna <me@hydev.org>
2020-10-23 13:38:13 -0400

    [+] Create links view controller with a table outlet

---
+++ ZoomLinkKeeper/LinksViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LinksViewController.swift
+//  ZoomLinkKeeper
+//
+//  Created by Hykilpikonna on 10/23/20.
+//
+
+import UIKit
+
+class LinksViewController: UIViewController
+{
+    @IBOutlet var table: UITableView!
+}
