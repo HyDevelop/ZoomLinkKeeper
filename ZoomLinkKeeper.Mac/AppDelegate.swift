69f7cbe7df43b71ce956e8a68594912b6a41aa93
Hykilpikonna <me@hydev.org>
2020-10-27 13:30:08 -0400

    [+] Create macOS branch

---
+++ ZoomLinkKeeper.Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ZoomLinkKeeper.Mac
+//
+//  Created by Hykilpikonna on 10/24/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ZoomLinkKeeper.Mac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ZoomLinkKeeper.Mac
+//
+//  Created by Hykilpikonna on 10/24/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
